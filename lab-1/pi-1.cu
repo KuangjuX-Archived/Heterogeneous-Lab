2e9b51bbbec7fecf7e5867cbf1b70a36b1048c9b
KuangjuX <18630816527@163.com>
2021-09-20 22:36:46 +0800

    complete lab-1

--- lab-1/pi-1.cu
+++ lab-1/pi-1.cu
@@ -1,29 +1,28 @@
-#include<stdio.h>
-#include<cuda.h>
-#include<cuda_runtime.h>
+#include <stdio.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
 
 #define BLOCK_NUM 32   //块数量
 #define THREAD_NUM 256 // 每个块中的线程数
-#define LOOP_N BLOCK_NUM * THREAD_NUM * 1000000
+#define LOOP_N ((unsigned long long)(BLOCK_NUM * THREAD_NUM * 100000))
 
-__global__ void leib_pi(double* g_sum) {
+__global__ void integral_pi(double *g_sum) {
     const int tid = threadIdx.x;
     const int bid = blockIdx.x;
     double tmp = 0;
-    int flag = -1;
+
     int idx = bid * THREAD_NUM + tid;
     int start = idx * 100000 + 1;
     int end = start + 100000;
-    for (int i = start; i < end; i++) {
-        tmp += flag * (1./(2 * i + 1));
-        flag = -flag;
+    for(int i = start; i < end; i++) {
+        tmp += 4. / (1 + ((i + 0.5) / LOOP_N) * ((i + 0.5) / LOOP_N));
     }
-    g_sum[bid*THREAD_NUM+tid] = tmp;
-}
+    g_sum[bid * THREAD_NUM + tid] = tmp;
+}   
 
-int main(){
+int main() {
     double *h_sum, *g_sum;
-    double pi_v = 1;
+    double pi_v = 0;
 
     // allocate host memory
     h_sum = (double*) malloc(sizeof(double) * BLOCK_NUM * THREAD_NUM);
@@ -32,7 +31,7 @@ int main(){
     cudaMalloc((void **)&g_sum, sizeof(double) * BLOCK_NUM * THREAD_NUM);
 
     // Execute kernels
-    leib_pi<<<BLOCK_NUM,THREAD_NUM>>>(g_sum);
+    integral_pi<<<BLOCK_NUM,THREAD_NUM>>>(g_sum);
 
     // Transfer output from device memory to host
     cudaMemcpy(h_sum, g_sum, sizeof(double)*BLOCK_NUM*THREAD_NUM, cudaMemcpyDeviceToHost);
@@ -41,8 +40,9 @@ int main(){
         pi_v += h_sum[i];
     }
 
-    printf("calculate %.10f\n", pi_v*4);
+    printf("calculate %.10f\n", (pi_v/LOOP_N));
 
     cudaFree(g_sum);
     free(h_sum);
+
 }
---
+++ lab-1/pi-2.cu
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+#define BLOCK_NUM 32   //块数量
+#define THREAD_NUM 256 // 每个块中的线程数
+#define LOOP_N BLOCK_NUM * THREAD_NUM * 1000000
+
+__global__ void leib_pi(double* g_sum) {
+    const int tid = threadIdx.x;
+    const int bid = blockIdx.x;
+    double tmp = 0;
+    int flag = -1;
+    int idx = bid * THREAD_NUM + tid;
+    int start = idx * 100000 + 1;
+    int end = start + 100000;
+    for (int i = start; i < end; i++) {
+        tmp += flag * (1./(2 * i + 1));
+        flag = -flag;
+    }
+    g_sum[bid*THREAD_NUM+tid] = tmp;
+}
+
+int main(){
+    double *h_sum, *g_sum;
+    double pi_v = 1;
+
+    // allocate host memory
+    h_sum = (double*) malloc(sizeof(double) * BLOCK_NUM * THREAD_NUM);
+
+    // Allocate device memory
+    cudaMalloc((void **)&g_sum, sizeof(double) * BLOCK_NUM * THREAD_NUM);
+
+    // Execute kernels
+    leib_pi<<<BLOCK_NUM,THREAD_NUM>>>(g_sum);
+
+    // Transfer output from device memory to host
+    cudaMemcpy(h_sum, g_sum, sizeof(double)*BLOCK_NUM*THREAD_NUM, cudaMemcpyDeviceToHost);
+
+    for (int i = 0; i < BLOCK_NUM * THREAD_NUM; i++) {
+        pi_v += h_sum[i];
+    }
+
+    printf("calculate %.10f\n", pi_v*4);
+
+    cudaFree(g_sum);
+    free(h_sum);
+}
